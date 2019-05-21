2ad40ccefb07725b51fb674bbb75d02d3a4a639e
yuda <33281225+yudaprama@users.noreply.github.com>
2019-05-22 00:49:41 +0700

    make in constant

--- lib/flutter_insets.dart
+++ lib/flutter_insets.dart
@@ -2,14 +2,14 @@ library flutter_insets;
 
 import 'package:flutter/material.dart';
 
-final Divider dividerIndent = const Divider(height: 0.0, indent: 16.0,);
-final Divider divider0 = const Divider(height: 0.0);
+const Divider dividerIndent = const Divider(height: 0.0, indent: 16.0,);
+const Divider divider0 = const Divider(height: 0.0);
 
-final SizedBox sizeBox25 = const SizedBox(height: 25);
-final SizedBox sizeBox15 = const SizedBox(height: 15);
-final SizedBox sizeBox10 = const SizedBox(height: 10);
-final SizedBox sizeBox5 = const SizedBox(height: 5);
+const SizedBox sizeBox25 = const SizedBox(height: 25);
+const SizedBox sizeBox15 = const SizedBox(height: 15);
+const SizedBox sizeBox10 = const SizedBox(height: 10);
+const SizedBox sizeBox5 = const SizedBox(height: 5);
 
-final SizedBox widthBox5 = const SizedBox(width: 5);
-final SizedBox widthBox10 = const SizedBox(width: 10);
-final SizedBox widthBox15 = const SizedBox(width: 15);
+const SizedBox widthBox5 = const SizedBox(width: 5);
+const SizedBox widthBox10 = const SizedBox(width: 10);
+const SizedBox widthBox15 = const SizedBox(width: 15);
