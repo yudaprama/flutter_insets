56f267e2b6f272051bc6a0c1c6b0f7c8ff08e557
yuda <33281225+yudaprama@users.noreply.github.com>
2019-05-22 00:30:39 +0700

    first commit

---
+++ lib/flutter_insets.dart
@@ -0,0 +1,15 @@
+library flutter_insets;
+
+import 'package:flutter/material.dart';
+
+final Divider dividerIndent = const Divider(height: 0.0, indent: 16.0,);
+final Divider divider0 = const Divider(height: 0.0);
+
+final SizedBox sizeBox25 = const SizedBox(height: 25);
+final SizedBox sizeBox15 = const SizedBox(height: 15);
+final SizedBox sizeBox10 = const SizedBox(height: 10);
+final SizedBox sizeBox5 = const SizedBox(height: 5);
+
+final SizedBox widthBox5 = const SizedBox(width: 5);
+final SizedBox widthBox10 = const SizedBox(width: 10);
+final SizedBox widthBox15 = const SizedBox(width: 15);
